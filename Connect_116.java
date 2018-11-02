7fc13b54e833e8b57d1e17674e1fbf546bc10ada
luocai <13540680230@163.com>
2018-11-02 21:02:26 +0800

    7/100

---
+++ Connect_116.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+class TreeLinkNode {
+      int val;
+      TreeLinkNode left, right, next;
+      TreeLinkNode(int x) { val = x; }
+}
+
+public class Connect_116 {
+	
+	public void connect(TreeLinkNode root) {
+        
+    }
+}
